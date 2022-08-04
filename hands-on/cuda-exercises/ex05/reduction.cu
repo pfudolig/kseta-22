d793f0262c7ceca209a5d0cc05444c570c700b1b
Paula Patrice Fudolig <pfudolig@patatrack02.cern.ch>
2022-08-04 14:15:19 +0200

    Partial Sums

--- hands-on/cuda-exercises/ex05/reduction.cu
+++ hands-on/cuda-exercises/ex05/reduction.cu
@@ -10,20 +10,29 @@ const int num_blocks = 4096;
 const int block_size = 64;
 
 // Part 1 of 6: implement the kernel
-__global__ void block_sum(const int* input, int* per_block_results,
-                          const size_t n)
+__global__ void block_sum(const int* input, int* per_block_results, const size_t n)
 {
-  __shared__ int sdata[num_blocks];
+  __shared__ int sdata[block_size];
   //int thread = threadIdx.x;
   int global_thread = threadIdx.x + blockIdx.x * blockDim.x;
-  //sdata[global_thread] = 0;
+  int s = blockDim.x / 2;
   if (global_thread < n) {
-    sdata[global_thread] = input[global_thread];
+    sdata[threadIdx.x] = input[global_thread];
     __syncthreads();
-    per_block_results[global_thread] += sdata[global_thread];
+    while (s > 0) {
+      if (threadIdx.x < s) {
+        sdata[threadIdx.x] = sdata[threadIdx.x] + sdata[s + threadIdx.x];
+        __syncthreads();
+      }
+      s = s/2;
+      printf("%d\n",s);
+    }
     __syncthreads();
+    if (threadIdx.x == 0) {
+      per_block_results[blockIdx.x] = sdata[0]; //save just the first element of our sdata array
+    }
   }
-
+  //printf("%d",per_block_results[blockIdx.x]);
 }
 
 __global__ void saxpy(unsigned int n, double a, double* x, double* y)
@@ -47,7 +56,8 @@ int main(void)
 
   // create array of 256ki elements
   const int num_elements = 1 << 18;
-  // generate random input on the host  std::vector<int> h_input(num_elements);
+  // generate random input on the host  
+  std::vector<int> h_input(num_elements);
   for (auto& elt : h_input) {
     elt = distrib(gen);
   }
@@ -64,46 +74,48 @@ int main(void)
   cudaMalloc((void**)&d_input,n);
   
   int* d_partial_sums_and_total;
-  cudaMalloc((void**)&d_partial_sums_and_total,block_size * sizeof(int)); //was block_size but idk
+  cudaMalloc((void**)&d_partial_sums_and_total,num_blocks * sizeof(int)); //make num_blocks?
 
   int* d_final;
-  cudaMalloc((void**)&d_final,0);
+  cudaMalloc((void**)&d_final,sizeof(int));
 
-  cudaEvent_t start,stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+  std::vector<int> host(num_blocks * sizeof(int)); 
+
+  //cudaEvent_t start,stop;
+  //cudaEventCreate(&start);
+  //cudaEventCreate(&stop);
 
   cudaMemcpy(d_input,h_input.data(),n,cudaMemcpyHostToDevice);
 
-  cudaEventRecord(start);
+  //cudaEventRecord(start);
 
-  block_sum<<<num_blocks, block_size>>>(d_input, d_partial_sums_and_total,
-                                        n);
+  block_sum<<<num_blocks, block_size>>>(d_input, d_partial_sums_and_total,n);
   cudaDeviceSynchronize();
-  cudaEventRecord(stop);
-  cudaEventSynchronize(stop);
-  //cudaMemcpy(h_input.data(),d_partial_sums_and_total,n,cudaMemcpyDeviceToHost);
+  //cudaEventRecord(stop);
+  //cudaEventSynchronize(stop);
+  cudaMemcpy(host.data(),d_partial_sums_and_total,num_blocks,cudaMemcpyDeviceToHost);
 
-/* Checking vectors
-  for (int i=0; i<h_input.size();i++) {
-    printf("%d\n",h_input[i]);
-  }
-  printf("%d\n",h_input.size());
-  printf("%d\n",h_input.begin());
-  printf("%d\n",h_input.end());
+ //Checking vectors
+ // for (int i=0; i<h_input.size();i++) {
+  //  printf("%d\n",h_input[i]);
+  //}
+  printf("%d\n",host.size()); //size is num elems when launch w/ n but should be num_blocks
+  //printf("%d\n",h_input.begin());
+  //printf("%d\n",h_input.end());
   //std::cout << *h_input.data() << std::endl;
-*/
 
+/*
   block_sum<<<1,num_blocks>>>(d_partial_sums_and_total,d_final,block_size*sizeof(int));
   cudaDeviceSynchronize();
   h_input.resize(0);
-  cudaMemcpy(h_input.data(),d_final,0,cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_input.data(),d_final,sineof(int),cudaMemcpyDeviceToHost);
 
   printf("%d\n",h_input.size());
   printf("%d\n",h_input.begin());
   printf("%d\n",h_input.end());
   //std::cout << "Device sum: " << h_input << std::endl;
-
+*/
+/*
   float milliseconds = 0;
   cudaEventElapsedTime(&milliseconds, start, stop); //write recorded time to milliseconds variable 
 
@@ -119,7 +131,7 @@ int main(void)
   cudaFree(d_input);
   cudaFree(d_partial_sums_and_total);
   cudaFree(d_final);
-
+*/
 
   return 0;
 }
